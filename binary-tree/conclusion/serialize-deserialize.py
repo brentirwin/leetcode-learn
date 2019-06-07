e52aa56aca8b5659d97cd70df0409f3be46b5a9d
Brent Irwin <brent@brentirwin.tv>
2019-06-07 16:53:08 -0500

    Conclusion 6 Test Suite

--- binary-tree/conclusion/serialize-deserialize.py
+++ binary-tree/conclusion/serialize-deserialize.py
@@ -8,6 +8,17 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
+class TreeNode(object):
+    def __init__(self, x):
+      self.val = x
+      self.left = None
+      self.right = None
+
+tree = TreeNode(1)
+tree.left = TreeNode(2)
+tree.right = TreeNode(3)
+tree.right.left = TreeNode(4)
+tree.right.right = TreeNode(5)
 
 class Codec:
 
@@ -31,25 +42,28 @@ class Codec:
                     queue.append(node.right)
                 else:
                     output += 'null'
-        return ','.join(output)
+        return ','.join(str(x) for x in output)
 
-    def deserialize(self, data):
-        """Decodes your encoded data to tree.
+    # def deserialize(self, data):
+    #     """Decodes your encoded data to tree.
         
-        :type data: str
-        :rtype: TreeNode
-        """
-        queue = []
-        tree = data.split(',')
-        root = TreeNode(None)
-        if root:
-            root = TreeNode(tree[0])
-            queue = 
-        while len(queue):
-            level = queue.copy()
-            queue = []
-            for node in level:
+    #     :type data: str
+    #     :rtype: TreeNode
+    #     """
+    #     queue = []
+    #     tree = data.split(',')
+    #     root = TreeNode(None)
+    #     if root:
+    #         root = TreeNode(tree[0])
+    #         queue = 
+    #     while len(queue):
+    #         level = queue.copy()
+    #         queue = []
+    #         for node in level:
 
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
 # codec.deserialize(codec.serialize(root))
+
+codec = Codec()
+print(codec.serialize(tree))
